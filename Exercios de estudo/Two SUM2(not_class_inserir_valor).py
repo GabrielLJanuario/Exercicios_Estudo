8092b81663f77097739a8442bd41740fd8746597
GabrielLJanuario <148555743+GabrielLJanuario@users.noreply.github.com>
2024-09-24 14:39:09 -0300

    atualização 3.0

--- Exercios de estudo/Two SUM2(not_class_inserir_valor).py
+++ Exercios de estudo/Two SUM2(not_class_inserir_valor).py
@@ -3,15 +3,13 @@ nums=[2,7,11,15]
 target=9
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        indice_resultado = []
+        indice_resultado = {}
 
-        for i, num1 in enumerate(nums):
-            for j in range(i + 1, len(nums)):
-                num2 = nums[j]
-                valor = num1 + num2
-                if valor == target:
-                    return [i, j]
-        return indice_resultado;
+        for i, num in enumerate(nums):
+            valor = target - num
+            if valor in indice_resultado:
+                return [indice_resultado[valor], i]
+            indice_resultado[num] = i
 
 
 
