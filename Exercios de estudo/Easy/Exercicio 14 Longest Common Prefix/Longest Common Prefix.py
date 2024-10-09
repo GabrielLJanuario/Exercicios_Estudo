a40ec5f1ee821bc6a22d7910a7a8afaac1196192
GabrielLJanuario <148555743+GabrielLJanuario@users.noreply.github.com>
2024-10-09 20:21:48 -0300

    reset

--- Exercios de estudo/Easy/Exercicio 14 Longest Common Prefix/Longest Common Prefix.py
+++ Exercios de estudo/Easy/Exercicio 14 Longest Common Prefix/Longest Common Prefix.py
@@ -7,14 +7,14 @@ class Solution:
         resposta=[]
         palavras=[list(strs) for strs in strs]
         menor_linha=min(palavras, key=len) # Encontrar a linha com o menor número de caracteres
-        for i in range(len(menor_linha)):
-            for j in range(len(palavras)):
+        for i in range(len(menor_linha)): # Percorre a quantidade de itens de menor linha
+            for j in range(len(palavras)): #Percorre a quantidade de itens de palavras
                 if palavras[j][i] != menor_linha[i]: # Verifica na menor lista se a letra que est no indice é igual a da lista palavras
-                    return ''.join(resposta)
-            resposta.append(menor_linha[i])
+                    return ''.join(resposta) # Retorna em txt a letra repetida da posição
+            resposta.append(menor_linha[i]) # Adiciona na lista a resposta
 
 
-        return ''.join(resposta)
+        return ''.join(resposta) # Retorna a resposta do enunciado
 
 
 
---
+++ Jornada Python/Dia 1/Codigo_dia_1.py
@@ -0,0 +1,23 @@
+# Passo 01 Entrar no sistema da Empres: Link do sistema da Empresa :https://dlp.hashtagtreinamentos.com/python/intensivao/login
+# Passo 02 Logar no sistema
+# Passo 03 Importar a base de dados
+# Passo 04 Cadastrar 1 produto
+# Passo 05 Repetir o processo ate finalizar
+
+import pyautogui
+import time
+
+pyautogui.PAUSE = 1 # Da uma pausa para executar o proximo comando
+pyautogui.press("win") # Comando para pressionar uma tecla (neste caso a teclah Win)
+pyautogui.write("opera") # Comando para escrever (neste caso escrever "chrome")
+pyautogui.press("enter")
+time.sleep(3)
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+pyautogui.press("enter")
+time.sleep(3)
+pyautogui.click(x=692, y=392) # Comando para clicar na posição especificada em x e y
+pyautogui.write("Gabriel.j@gmail.com")
+pyautogui.press("tab")
+pyautogui.write("123456")
+pyautogui.press("tab")
+pyautogui.press("enter")
